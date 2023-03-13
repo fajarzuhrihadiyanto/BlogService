17ddc13a54370c08e09966c401980e8ce69ada1e
Fajar Zuhri H <fajarzuhri76@gmail.com>
2023-03-13 14:16:21 +0700

    feat: adjust password field length in users model

--- models/user.go
+++ models/user.go
@@ -9,7 +9,7 @@ import (
 type User struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
 	Email     string    `json:"email" validate:"required,email" gorm:"type:varchar(255);unique;not null"`
-	Password  string    `json:"password" validate:"required" gorm:"type:varchar(40);not null"`
+	Password  string    `json:"password" validate:"required" gorm:"type:varchar(64);not null"`
 	Name      string    `json:"name" validate:"required" gorm:"type:varchar(50);not null"`
 	CreatedAt time.Time `json:"created_at" gorm:"type:datetime"`
 	UpdatedAt time.Time `json:"updated_at"`
